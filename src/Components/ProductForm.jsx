70d763c431283137e0b4ed7d8bcec8d2773370a0
Dharmendra Patel <dharemendra@mixinstechnology.onmicrosoft.com>
2026-01-04 18:08:42 +0530

    final commit

--- src/Components/ProductForm.jsx
+++ src/Components/ProductForm.jsx
@@ -190,20 +190,20 @@ else{
       {mode=='add' &&
       <>
        <label className='mx-1 text-xl text-zinc-800 '>Photo</label>
-       <div className={ "photo w-100 mx-1 mb-2 h-40 border-dotted border-2 border-zinc-700 text-center relative" }>
+       <div className={ "photo max-w-100 w-auto mx-1 mb-2 h-40 border-dotted border-2 border-zinc-700 text-center relative" }>
          <input 
           type="file" 
           accept=".jpg,.jpeg,.png,.webp"
           className={form.image==''?
-          'w-100  h-44 text-center py-[15%] px-[20%]':              //to show Choose file text
-          'w-100  h-44 text-center py-[15%] px-[20%] text-transparent' //to hide Choose file text when image uploaded
+          'w-100  h-44 text-center py-[16%] pl-[10vw]':              //to show Choose file text
+          ' text-transparent' //to hide Choose file text when image uploaded
           }
           multiple={false} 
           onChange={(e)=>Setform((prev)=>({...prev,image:URL.createObjectURL(e.target.files[0])}))} 
           />
 
         {form.image!==''&& 
-        <img className={ 'w-60 p-3 h-40 mx-[18%] absolute top-0 left-0'} src={form.image} alt="" />
+        <img className={ '  w-60 p-3 h-40 ml-[4vw] absolute top-0 left-0'} src={form.image} alt="" />
         }
        </div>
        </>}
